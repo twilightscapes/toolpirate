afec6fa67bb7fe82fd00cfcb8256fc26dbe89464
Todd <ihatespam@gmail.com>
2024-09-09 08:30:56 -0500

    initial

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Pirate",
+	author: "TOOL",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en",
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Meta property used as the default description meta property
-	description: "PIRATE - social media for the people by the people",
+	description: "Tool - Videos",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -22,7 +22,7 @@ export const siteConfig: SiteConfig = {
 	// Option to sort posts by updatedDate if set to true (if property exists). Default (false) will sort by publishDate
 	sortPostsByUpdatedDate: false,
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Pirate",
+	title: "TOOL",
 	webmentions: {
 		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-pirate.netlify.app/posts/webmentions/
 		link: "",
